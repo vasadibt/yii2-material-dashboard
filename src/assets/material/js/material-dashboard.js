1ca3c12e48a42a83b8c248ac88f4aa335e5d1ab1
Tamás Vasadi-Balogh <vasadibt@gmail.com>
2022-01-05 09:18:15 +0100

    Fix js in material

--- src/assets/material/js/material-dashboard.js
+++ src/assets/material/js/material-dashboard.js
@@ -15,20 +15,6 @@
 
  */
 
-(function () {
-    isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
-
-    if (isWindows) {
-        // if we are on windows OS we activate the perfectScrollbar function
-        $('.sidebar .sidebar-wrapper, .main-panel').perfectScrollbar();
-
-        $('html').addClass('perfect-scrollbar-on');
-    } else {
-        $('html').addClass('perfect-scrollbar-off');
-    }
-})();
-
-
 var breakCards = true;
 
 var searchVisible = 0;
